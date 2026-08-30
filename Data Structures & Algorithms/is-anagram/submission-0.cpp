01c76fcea0896d58175c0d45c46886eb89696367
anand201r-del <anand201r@gmail.com>
2026-08-30 18:27:44 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int freqS[26] = {0};
+        int freqT[26] = {0};
+
+        for (int i = 0; i < s.length(); i++) {
+            freqS[s[i] - 'a']++;
+            freqT[t[i] - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freqS[i] != freqT[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
